5cebd7849b03cb5f4afeea2e41f320aa1aaa987b
Neil Gat <neilsantgat@college.harvard.edu>
2015-12-24 23:36:30 -0800

    added python3 and common python3 libraries

--- InitializeVM/initialize.sh
+++ InitializeVM/initialize.sh
@@ -1,10 +1,17 @@
-apt-get update
-apt-get dist-upgrade
+apt-get -y update
+apt-get -y dist-upgrade
 
-apt-get install emacs24
-apt-get install tmux
+apt-get -y install emacs24
+apt-get -y install tmux
 
-apt-get install wget
+apt-get -y install wget
 wget https://dl.dropboxusercontent.com/u/65169988/.tmux.conf
 
-apt-get install python3.4
+apt-get -y install python3.4
+apt-get -y install python3-pip
+apt-get -y install python3-bs4
+apt-get -y install python3-requests
+apt-get -y install python3-numpy
+apt-get -y install python3-scipy
+apt-get -y install python3-matplotlib
+apt-get -y install python3-pandas
