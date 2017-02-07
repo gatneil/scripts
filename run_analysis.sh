22ce352beca0c9c0e83be61e18e06be958f36b76
negat <negat@microsoft.com>
2017-02-07 04:30:34 +0000

    updated analysis script

--- run_analysis.sh
+++ run_analysis.sh
@@ -13,7 +13,12 @@ python -c "import nltk; nltk.download('punkt')"
 wget https://tr24seven.blob.core.windows.net/text/7.tar.gz
 tar -xzvf 7.tar.gz
 grep -R "" 7/ | cut -d ":" -f 2 > text.txt
-tail -n +$RANDOM text.txt | head -1000 > sampleText.txt
+r=$RANDOM
+tail -n +$r text.txt | head -1000 > sampleText.txt
 
 # do analysis
-sumy lex-rank --length=10 --file=sampleText.txt > res$RANDOM.txt
+sumy lex-rank --length=10 --file=sampleText.txt > res$r.txt
+
+wget https://raw.githubusercontent.com/gatneil/scripts/master/tmp
+
+scp res$r.txt negat@13.76.81.242:~/res/ -i tmp
