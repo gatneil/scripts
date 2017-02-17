d204dbce62b5a1baeaba3b371012619378edc403
negat <negat@microsoft.com>
2017-02-17 19:06:55 +0000

    initial checkin of install_count_requests.sh

---
+++ install_count_requests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -evx
+
+# install flask
+apt-get update
+apt-get upgrade -y
+apt-get install python-pip -y
+pip install flask
+
+# download and run the app that will count requests
+wget https://raw.githubusercontent.com/gatneil/scripts/master/count_requests.py
+FLASK_APP=count_requests.py flask run --host=0.0.0.0
+
