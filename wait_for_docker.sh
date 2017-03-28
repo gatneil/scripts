7a8661aafbfc8367425a210a2459ee690ec6e69c
negat <negat@microsoft.com>
2017-03-28 21:57:36 +0000

    made wait_for_docker actually wait for docker

--- wait_for_docker.sh
+++ wait_for_docker.sh
@@ -1,3 +1,13 @@
-#!/bin/bash
+status_file_path=/var/lib/waagent/$(ls /var/lib/waagent | grep Microsoft.Azure.Extensions.Docker | grep -v xml)/status/0.status
 
-echo 'hi'
+while [ 1 == 1 ]
+do
+	if grep -q "success" $status_file_path; then
+		break
+	else
+		sleep 5
+		echo "waiting"
+	fi
+done
+
+echo 'docker finished; do cool stuff here'
