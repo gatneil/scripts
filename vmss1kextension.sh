932d3f50d175c00423567dc184f2a75b795ace38
negat <negat@microsoft.com>
2017-04-11 01:42:35 +0000

    nsg not on scale set

--- vmss1kextension.sh
+++ vmss1kextension.sh
@@ -28,6 +28,6 @@ pip=`az network public-ip show --resource-group $rgname --name "$vmname"PublicIP
 echo $pip
 
 
-az vmss create --resource-group $rgname --name $vmssname --image Canonical:UbuntuServer:16.04-LTS:latest --admin-username azureuser --ssh-key-value $pubkeypath --nsg "" --upgrade-policy-mode Automatic --instance-count 5
+az vmss create --resource-group $rgname --name $vmssname --image Canonical:UbuntuServer:16.04-LTS:latest --admin-username azureuser --ssh-key-value $pubkeypath --upgrade-policy-mode Automatic --instance-count 5
 
 az vmss extension set --publisher "Microsoft.Azure.Extensions" --name "CustomScript" --resource-group $rgname --vmss-name $vmssname --settings "{\"fileUris\": [\"${uriBase}checkin.sh\"], \"commandToExecute\": \"bash checkin.sh ${pip}\"}"
