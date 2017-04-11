73b6b8f371310a818f80376867bef0e573375644
negat <negat@microsoft.com>
2017-04-11 00:26:49 +0000

    temporary undo

--- count_requests.py
+++ count_requests.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/checkin/<vm_id>")
+@app.route("/")
 def hello():
-    return vm_id
+    return "hello"
 
     if __name__ == "__main__":
             app.run()
