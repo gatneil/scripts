685aa1a297eb8432d96ca782b94c64184bd9c7cc
negat <negat@microsoft.com>
2017-04-11 00:56:25 +0000

    added route

--- count_requests.py
+++ count_requests.py
@@ -4,3 +4,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!!!!!!'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
